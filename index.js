8ea51362ec6344ecb64fcb2f4b26ab6067206d56
Connor Cowan <ccowan003@comcast.net>
2019-03-28 03:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-happyHolidays()
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy holidays, ${name}!'
+}
+
+function happyHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function happyHolidayCountdown(days, holiday) {
+  return 'Its ${days} days until${holiday}!'
+}
